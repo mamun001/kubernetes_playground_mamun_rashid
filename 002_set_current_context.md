92857466ca9b0f5c63550c05baa5eeb1e1753a65
Mamun Rashid <mamun001@gmail.com>
2022-05-09 14:09:17 -0700

    May 09 2022

---
+++ 002_set_current_context.md
@@ -0,0 +1,12 @@
+
+
+# TASK: set context w/o using kubectx tool
+
+
+##  ANSWER:
+
+### kubectl config use-context foobar
+#### to confirm
+### kubectl config get-contexts | grep ^\*
+
+
--- 009_create_a_GKE_cluster_in_gcp.sh
+++ 009_create_a_GKE_cluster_in_gcp.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 #  TASK:
 # Creates a GKE Kubernetes cluster in your GCP project from bash
@@ -6,20 +5,9 @@
 # Pre-requisite: you have to be logged to GCP on your shell and you have to have a default project set.
 
 
-#  Bash script to create a GKE cluster in GCP
+#  ANSWER
 
-echo _________________________________________________________________________
-echo creating gke cluster on current project:
-echo
-sleep 3
-echo
-echo ENTER CLUSTER_NAME: underscore_not_allowed: 
-read CLUSTER
-echo ENTER REGION: eg: us-central1:
-read REGION
-echo ENTER NODE_COUNT:
-read NODE_COUNT
-gcloud container clusters create $CLUSTER --region $REGION --num-nodes $NODE_COUNT
-echo 
-echo should be done
+# Note: underscore_not_allowed:
+# Region:  e.g: us-central1:
+gcloud container clusters create foobar_nameR --region foobar_region --num-nodes foobar_node_count
 
